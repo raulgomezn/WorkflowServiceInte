75da60c376d89da1e0c6426dc2a5aa7b5e1835bd
raulgomezn <raul.gomezn@hotmail.com>
2017-10-30 00:12:17 -0500

    Definicion de la entida y el servicio, reestructuración

---
+++ WorkflowServiceInte/Activities/ConsumeRestGetActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using LightCaseClient;
+using System.Configuration;
+using WorkflowServiceInte.Entities;
+using NLog.Fluent;
+using System.Activities.Tracking;
+
+namespace WorkflowServiceInte.Activities
+{
+
+    public sealed class ConsumeRestGetActivity : CodeActivity
+    {
+        // Defina un argumento de entrada de actividad de tipo string
+        public InArgument<WorkflowEntity> Data { get; set; }
+
+        // Si la actividad devuelve un valor, se debe derivar de CodeActivity<TResult>
+        // y devolver el valor desde el método Execute.
+        protected override void Execute(CodeActivityContext context)
+        {
+            Log.Debug("Inicio Actividad.");
+            // Obtenga el valor de tiempo de ejecución del argumento de entrada Text
+            string GmapKey = ConfigurationManager.AppSettings["GmapKey"];
+            string BmapKey = ConfigurationManager.AppSettings["BmapKey"];
+
+            WorkflowEntity dataEntry = context.GetValue(this.Data);
+
+
+
+
+            /*CustomTrackingRecord record = new CustomTrackingRecord("MyRecord-Get-Init");
+            record.Data.Add(new KeyValuePair<String, Object>("ExecutionTime", DateTime.Now));
+            context.Track(record);*/
+        }
+    }
+}
---
+++ WorkflowServiceInte/Entities/WorkflowEntity.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkflowServiceInte.Entities
+{
+    public class WorkflowEntity
+    {
+        /// <summary>
+        /// Campo Url
+        /// </summary>
+        private string[] urls;
+
+        /// <summary>
+        /// Propiedad Url
+        /// </summary>
+        public string[] Urls
+        {
+            get { return urls; }
+            set { urls = value; }
+        }
+
+        /// <summary>
+        /// Campo Method
+        /// </summary>
+        private string[] method;
+
+        /// <summary>
+        /// Propiedad Method
+        /// </summary>
+        public string[] Method
+        {
+            get { return method; }
+            set { method = value; }
+        }
+
+        /// <summary>
+        /// Campo Body
+        /// </summary>
+        private string[] body;
+
+        /// <summary>
+        /// Propiedad Body
+        /// </summary>
+        public string[] Body
+        {
+            get { return body; }
+            set { body = value; }
+        }
+
+        /// <summary>
+        /// Campo properties (Gmaps, Bmaps etc)
+        /// </summary>
+        private string[] properties;
+
+        /// <summary>
+        /// Propiedad properties
+        /// </summary>
+        public string[] Properties
+        {
+            get { return properties; }
+            set { properties = value; }
+        }
+
+        /// <summary>
+        /// Campo latOrigins
+        /// </summary>
+        private Decimal[] latOrigins;
+
+        /// <summary>
+        /// Propiedad LatiduteOrigins
+        /// </summary>
+        public Decimal[] LatitudeOrigins
+        {
+            get { return latOrigins; }
+            set { latOrigins = value; }
+        }
+
+        /// <summary>
+        /// Campo longitudeOrigins
+        /// </summary>
+        private Decimal[] longitudeOrigins;
+
+        /// <summary>
+        /// Propiedad LongitudeOrigins
+        /// </summary>
+        public Decimal[] LongitudeOrigins
+        {
+            get { return longitudeOrigins; }
+            set { longitudeOrigins = value; }
+        }
+
+        /// <summary>
+        /// Campo latiduteDestinations
+        /// </summary>
+        private Decimal[] latitudeDestinations;
+
+        /// <summary>
+        /// Propiedad LatiduteDestinations
+        /// </summary>
+        public Decimal[] LatitudeDestinations
+        {
+            get { return latitudeDestinations; }
+            set { latitudeDestinations = value; }
+        }
+
+        /// <summary>
+        /// Campo  LongitudeDestinations
+        /// </summary>
+        private Decimal[] longitudeDestinations;
+
+        /// <summary>
+        /// Propiedad LongitudeDestinations
+        /// </summary>
+        public Decimal[] LongitudeDestinations
+        {
+            get { return longitudeDestinations; }
+            set { longitudeDestinations = value; }
+        }
+
+        /// <summary>
+        /// Campo IdPlaces
+        /// </summary>
+        private string[] idPlaces;
+
+        /// <summary>
+        /// Propiedad IdPlaces
+        /// </summary>
+        public string[] IdPlaces
+        {
+            get { return idPlaces; }
+            set { idPlaces = value; }
+        }
+    }
+}
